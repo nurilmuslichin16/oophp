795c2b583d07c62bac4c33cd82c868aeb4b68805
nurilmuslichin16 <nurilmuslichin16@gmail.com>
2019-05-22 06:44:47 +0700

    Mengupload ke GitHub

---
+++ constant.php
@@ -0,0 +1,20 @@
+<?php 
+
+// define('NAMA', 'Nuril Muslichin');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+// class Coba {
+// 	const NAMA = "Nuril";
+// }
+
+// echo Coba::NAMA;
+
+echo __TRAIT__;
+echo __CLASS__;
+
+ ?>
